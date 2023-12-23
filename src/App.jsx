9d66f3a7f1416893185d5c143575084b96f92780
uddeshya amit <uddeshyasrivastava05@gmail.com>
2023-12-24 01:19:58 +0530

    Add animated greeting to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 function App() {
-	return <div>Welcome to my portfolio</div>;
+	return <div>
+    <iframe src="https://giphy.com/embed/bcKmIWkUMCjVm" width="480" height="349" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/animated-hello-waving-bcKmIWkUMCjVm">via GIPHY</a></p>
+  </div>;
 }
 
 export default App;
