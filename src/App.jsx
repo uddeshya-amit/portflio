7dea01f1adb601fbe7d9a01895c75c55bc7cc557
uddeshya amit <uddeshyasrivastava05@gmail.com>
2023-12-24 12:23:37 +0530

    Add Tailwind CSS configuration and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 function App() {
-	return <div>
-    <iframe src="https://giphy.com/embed/bcKmIWkUMCjVm" width="480" height="349" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/animated-hello-waving-bcKmIWkUMCjVm">via GIPHY</a></p>
-  </div>;
+	return (
+		<div
+			// center the content inside div
+      className="flex flex-col items-center justify-center h-screen bg-gray-900"
+			>
+			<iframe
+				src="https://giphy.com/embed/bcKmIWkUMCjVm"
+				width="480"
+				height="349"
+				frameBorder="0"
+				class="giphy-embed"
+				allowFullScreen></iframe>
+			<p>
+				<a href="https://giphy.com/gifs/animated-hello-waving-bcKmIWkUMCjVm">
+					via GIPHY
+				</a>
+			</p>
+		</div>
+	);
 }
 
 export default App;
